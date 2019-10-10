8b366b6fbd597cf31b1d9354d2ff1ee1c9a6c01d
Sam Levy <slevy239@gmail.com>
2019-10-09 20:52:00 -0400

    link js and a tags

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,3 +1,32 @@
+console.log("carosel")
+  // Activate Carousel
+  $("#myCarousel").carousel({
+    interval: 10000
+  });
+  
+  // Enable Carousel Indicators
+  $(".item1").click(function () {
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function () {
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function () {
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function () {
+    $("#myCarousel").carousel(3);
+  });
+  
+  // Enable Carousel Controls
+  $(".left").click(function () {
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function () {
+    $("#myCarousel").carousel("next");
+  });
+  
+  
 
 (function($) {
 
@@ -83,91 +112,68 @@
       });
   });
 
-    // Activate Carousel
-    $("#myCarousel").carousel();
 
-    // Enable Carousel Indicators
-    $(".item1").click(function () {
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function () {
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function () {
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function () {
-        $("#myCarousel").carousel(3);
-    });
 
-    // Enable Carousel Controls
-    $(".left").click(function () {
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function () {
-        $("#myCarousel").carousel("next");
-    });
-    
 
 })(jQuery);
 
 
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyBzT9Ll9BfZ3ZSOUCm1WKvXVMF9CPKtCjc",
-  authDomain: "portfolio-a089f.firebaseapp.com",
-  databaseURL: "https://portfolio-a089f.firebaseio.com",
-  projectId: "portfolio-a089f",
-  storageBucket: "",
-  messagingSenderId: "617932475716",
-  appId: "1:617932475716:web:85b094abcd8664d069db11"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBzT9Ll9BfZ3ZSOUCm1WKvXVMF9CPKtCjc",
+//   authDomain: "portfolio-a089f.firebaseapp.com",
+//   databaseURL: "https://portfolio-a089f.firebaseio.com",
+//   projectId: "portfolio-a089f",
+//   storageBucket: "",
+//   messagingSenderId: "617932475716",
+//   appId: "1:617932475716:web:85b094abcd8664d069db11"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 
-var database = firebase.database();
+// var database = firebase.database();
 
-$("#submit").on("click", function (event) {
-  event.preventDefault();
+// $("#submit").on("click", function (event) {
+//   event.preventDefault();
 
-  var first = $("#first").val().trim();
-  var last = $("#last").val().trim();
-  var email = $("#email").val().trim();
-  var comment = $("#comment").val().trim();
-  var time = moment().format('LLL');
+//   var first = $("#first").val().trim();
+//   var last = $("#last").val().trim();
+//   var email = $("#email").val().trim();
+//   var comment = $("#comment").val().trim();
+//   var time = moment().format('LLL');
 
-  var newInput = {
-    newFirst: first,
-    newLast: last,
-    newEmail: email,
-    newComment: comment,
-    inputTime: time,
-  };
+//   var newInput = {
+//     newFirst: first,
+//     newLast: last,
+//     newEmail: email,
+//     newComment: comment,
+//     inputTime: time,
+//   };
 
-  if (first === "") {
-    alert("Please enter your name!")
-  } else if (last === "") {
-    alert("Please enter your last name!")
-  } else if (email === "") {
-    alert("Please enter your email!")
-  } else if (comment === "") {
-    alert("Please enter a Comment or Question!")
-  } else {
-    // alert("Thank you for your input!");
-    $("#input").html("Thank You for your input!")
-  }
+//   if (first === "") {
+//     alert("Please enter your name!")
+//   } else if (last === "") {
+//     alert("Please enter your last name!")
+//   } else if (email === "") {
+//     alert("Please enter your email!")
+//   } else if (comment === "") {
+//     alert("Please enter a Comment or Question!")
+//   } else {
+//     // alert("Thank you for your input!");
+//     $("#input").html("Thank You for your input!")
+//   }
 
-  database.ref().push(newInput);
-
-
-
-  // $("#first").val("");
-  // $("#last").val("");
-  // $("#email").val("");
-  // $("#comment ").val("");
+//   database.ref().push(newInput);
 
 
-})
+
+//   // $("#first").val("");
+//   // $("#last").val("");
+//   // $("#email").val("");
+//   // $("#comment ").val("");
+
+
+// })
 
